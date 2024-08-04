f09e06c8dc493c7c35fd5633a4320d3949f93f4c
Jon Kinney <jonkinney@gmail.com>
2024-08-04 16:21:21 -0500

    lazy: update Lazy's UI w border & lighter backdrop
    Because of my colorscheme, the Lazy UI looked like it was overflowing
    the rest of my editor and blending in. Could be an issue with Nord, but
    adding a border seems like a fine setting no matter the colorscheme.

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -16,7 +16,7 @@ require("lazy").setup({
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
   install = { colorscheme = { "astrotheme", "habamax" } },
-  ui = { backdrop = 100 },
+  ui = { backdrop = 90, border = "single" },
   performance = {
     rtp = {
       -- disable some rtp plugins, add more to your liking
